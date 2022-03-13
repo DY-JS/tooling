7b9ed951a6aa10f335f3a552dc041f2ac3c4ccc4
DY-JS <delo1604@ukr.net>
2022-03-13 23:01:25 +0200

    Add dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,9 @@ module.exports = {
     filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
   },
+  devServer: {
+    port: 4200,
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./index.html",
