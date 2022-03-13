58154ee2dc09eab6bee0a085840a9bb5fd77c18c
DY-JS <delo1604@ukr.net>
2022-03-13 22:03:08 +0200

    Add asset setup

--- webpack.config.js
+++ webpack.config.js
@@ -12,4 +12,16 @@ module.exports = {
     filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
 };
